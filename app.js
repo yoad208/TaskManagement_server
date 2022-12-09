78c1b88b4e8e4f43040412f9a5056bea376c1b15
yoad208 <94039515+yoad208@users.noreply.github.com>
2022-12-09 13:56:25 +0200

    100%

--- app.js
+++ app.js
@@ -3,14 +3,12 @@ const cors = require('cors')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const cron = require('node-cron')
-const compression = requier('compression')
 require('./db/mongoConnect')
 
 const app = express()
 
 const PORT = 3001 | process.env.PORT
 
-app.use(compression())
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors())
